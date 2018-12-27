db84c0ab40e1ae6c4710d17c2c8478dc67cfa36d
Nadine <nad4ne@gmail.com>
2018-12-26 20:49:46 -0800

    createStore and authReducer

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
+
 import App from './components/App';
 
+const store = createStore(() => [], {}, applyMiddleware());
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+  <Provider><App /></Provider>, document.querySelector('#root'));
